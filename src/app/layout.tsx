91be73297e372a87db44638aabc82ae95e6fca80
Aleksandr Salnikov <TaynbIy@gmail.com>
2025-03-22 21:04:02 +0500

    2104

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,22 +16,28 @@ export default function RootLayout({
 }>) {
 	return (
 		<html className='overflow-x-hidden scroll-smooth' lang='ru'>
-			<body className='pb-14 overflow-x-hidden min-h-screen flex flex-col'>
-				<NextTopLoader
-					color='#fff'
-					initialPosition={0.08}
-					crawlSpeed={200}
-					height={3}
-					crawl={true}
-					showSpinner={false}
-					easing='ease'
-					speed={200}
-					shadow='0 0 10px #2299DD,0 0 5px #2299DD'
-				/>
-				<Navbar />
-				<SmoothScroll>{children}</SmoothScroll>
-				<Footer />
-			</body>
+			{process.env.NEXT_PUBLIC_IS_PAYED === 'true' ? (
+				<body className='pb-14 overflow-x-hidden min-h-screen flex flex-col'>
+					<NextTopLoader
+						color='#fff'
+						initialPosition={0.08}
+						crawlSpeed={200}
+						height={3}
+						crawl={true}
+						showSpinner={false}
+						easing='ease'
+						speed={200}
+						shadow='0 0 10px #2299DD,0 0 5px #2299DD'
+					/>
+					<Navbar />
+					<SmoothScroll>{children}</SmoothScroll>
+					<Footer />
+				</body>
+			) : (
+				<body className='h-screen flex justify-center items-center'>
+					<h1 className='text-center'>Работа сайта приостановлена</h1>
+				</body>
+			)}
 		</html>
 	)
 }
